e64ae6d4d303fc4029d5bf5373c884d54c9e825d
Qingyu <bitdqy@hotmail.com>
2020-03-26 16:19:44 +0800

    feat: add html parser

---
+++ src/index.ts
@@ -0,0 +1,76 @@
+type Markup = {
+  start: number;
+  end: number;
+  startTag: string;
+  type: string;
+};
+
+type BlockNode = {
+  text: string;
+  markups: Markup[];
+};
+
+function createFragmentFromStr(htmlStr: string) {
+  const fragment = document.createElement("div");
+  fragment.innerHTML = htmlStr;
+  return fragment;
+}
+
+function extractOpeningTagFromElement(elem: HTMLElement): string {
+  const START_TAG_REGEX = /<[a-z]+.*?>/;
+  const outerHTML = elem.outerHTML;
+  const startTagMatch = START_TAG_REGEX.exec(outerHTML);
+  if (startTagMatch === null) {
+    console.error("Cannot find start tag for node", outerHTML);
+    throw new Error("Cannot find start tag for node");
+  }
+  return startTagMatch[0];
+}
+
+export function parseHTMLStringToNode(htmlStr: string): BlockNode {
+  const fragment = createFragmentFromStr(htmlStr);
+
+  function parseHTMLElementToBlockNode(elem: HTMLElement): BlockNode {
+    function addOffsetForMarkupInsideElement(markups: Markup[]): Markup[] {
+      return markups.map(markup => {
+        return Object.assign({}, markup, {
+          start: currCol + markup.start - 1,
+          end: currCol + markup.end - 1
+        });
+      });
+    }
+    let text = "";
+    const markups = [];
+    let currCol = 1;
+    for (const child of Array.from(elem.childNodes)) {
+      if (!child.textContent) {
+        continue;
+      }
+      if (child.nodeType === Node.ELEMENT_NODE) {
+        const markupsInChildNodes =
+          child.childNodes.length > 0
+            ? parseHTMLElementToBlockNode(child as HTMLElement).markups
+            : [];
+
+        const markupInCurrentChild: Markup = {
+          start: currCol,
+          end: currCol + child.textContent.length - 1,
+          type: child.nodeName.toLowerCase(),
+          startTag: extractOpeningTagFromElement(child as HTMLElement)
+        };
+        markups.push(
+          markupInCurrentChild,
+          ...addOffsetForMarkupInsideElement(markupsInChildNodes)
+        );
+      }
+      text += child.textContent;
+      currCol += child.textContent.length;
+    }
+    return {
+      text,
+      markups
+    };
+  }
+
+  return parseHTMLElementToBlockNode(fragment);
+}
---
+++ test/parser.spec.ts
@@ -0,0 +1,57 @@
+import { parseHTMLStringToNode } from "../src";
+
+describe("Parser", () => {
+  it("should parse text correctly", () => {
+    const node = parseHTMLStringToNode("hello world");
+
+    expect(node.text).toBe("hello world");
+  });
+
+  it("should parse inline tag into markup", () => {
+    const node = parseHTMLStringToNode(
+      `hello <span class="hello world">wor</span>ld`
+    );
+
+    expect(node.text).toBe("hello world");
+    expect(node.markups[0]).toEqual({
+      start: 7,
+      end: 9,
+      startTag: `<span class="hello world">`,
+      type: "span"
+    });
+  });
+
+  it("should parse nested inline tag correctly", () => {
+    const node = parseHTMLStringToNode(
+      `hello <span class="hello world">w<b>o</b>r</span>ld`
+    );
+    expect(node.text).toBe("hello world");
+    expect(node.markups[0]).toEqual({
+      start: 7,
+      end: 9,
+      startTag: `<span class="hello world">`,
+      type: "span"
+    });
+
+    expect(node.markups[1]).toEqual({
+      start: 8,
+      end: 8,
+      startTag: `<b>`,
+      type: "b"
+    });
+  });
+
+  it('should parse nested tag in the head of string correctly', () => {
+    const node = parseHTMLStringToNode(
+      `<b>hell</b>o world`
+    );
+
+    expect(node.text).toBe("hello world");
+    expect(node.markups[0]).toEqual({
+      start: 1,
+      end: 4,
+      startTag: `<b>`,
+      type: "b"
+    });
+  })
+});
