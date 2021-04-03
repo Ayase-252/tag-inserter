319159e2a9dbfd3608301dd0af11c035471bfb4d
Qingyu Deng <bitdqy@hotmail.com>
2021-04-03 21:52:23 +0800

    refactor: extract parseHTMLElementToBlockNode to parser

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,10 @@
 import { BlockNode } from "./types";
-import Markup from "./markup";
-import { extractStartTagFromElement } from "./parser";
+import { parseHTMLElementToBlockNode } from "./parser";
 
 function createFragmentFromStr(htmlStr: string) {
   const fragment = document.createElement("div");
   fragment.innerHTML = htmlStr;
   return fragment;
-}
-
-function parseHTMLElementToBlockNode(elem: HTMLElement): BlockNode {
-  function addOffsetForNestedMarkup(markups: Markup[]): Markup[] {
-    return markups.map((markup) => {
-      markup.start = currCol + markup.start;
-      markup.end = currCol + markup.end;
-      return markup;
-    });
-  }
-  let text = "";
-  const markups: Markup[] = [];
-  let currCol = 0;
-  for (const child of Array.from(elem.childNodes)) {
-    if (!child.textContent) {
-      continue;
-    }
-    if (child.nodeType === Node.ELEMENT_NODE) {
-      const markupsInChildNodes =
-        child.childNodes.length > 0
-          ? parseHTMLElementToBlockNode(child as HTMLElement).markups
-          : [];
-
-      const markupInCurrentChild = new Markup({
-        start: currCol,
-        end: currCol + child.textContent.length - 1,
-        type: child.nodeName.toLowerCase(),
-        startTag: extractStartTagFromElement(child as HTMLElement),
-      });
-      markups.push(
-        markupInCurrentChild,
-        ...addOffsetForNestedMarkup(markupsInChildNodes)
-      );
-    }
-    text += child.textContent;
-    currCol += child.textContent.length;
-  }
-  return {
-    text,
-    markups,
-  };
 }
 
 export function parseHTMLStringToNode(htmlStr: string): BlockNode {
--- src/parser.ts
+++ src/parser.ts
@@ -1,4 +1,7 @@
-export function extractStartTagFromElement(elem: HTMLElement): string {
+import { BlockNode } from "./types";
+import Markup from "./markup";
+
+function extractStartTagFromElement(elem: HTMLElement): string {
   const START_TAG_REGEX = /<[a-z]+.*?>/;
   const outerHTML = elem.outerHTML;
   const startTagMatch = START_TAG_REGEX.exec(outerHTML);
@@ -7,4 +10,45 @@ export function extractStartTagFromElement(elem: HTMLElement): string {
     throw new Error("Cannot find start tag for node");
   }
   return startTagMatch[0];
+}
+
+export function parseHTMLElementToBlockNode(elem: HTMLElement): BlockNode {
+  function addOffsetForNestedMarkup(markups: Markup[]): Markup[] {
+    return markups.map((markup) => {
+      markup.start = currCol + markup.start;
+      markup.end = currCol + markup.end;
+      return markup;
+    });
+  }
+  let text = "";
+  const markups: Markup[] = [];
+  let currCol = 0;
+  for (const child of Array.from(elem.childNodes)) {
+    if (!child.textContent) {
+      continue;
+    }
+    if (child.nodeType === Node.ELEMENT_NODE) {
+      const markupsInChildNodes =
+        child.childNodes.length > 0
+          ? parseHTMLElementToBlockNode(child as HTMLElement).markups
+          : [];
+
+      const markupInCurrentChild = new Markup({
+        start: currCol,
+        end: currCol + child.textContent.length - 1,
+        type: child.nodeName.toLowerCase(),
+        startTag: extractStartTagFromElement(child as HTMLElement),
+      });
+      markups.push(
+        markupInCurrentChild,
+        ...addOffsetForNestedMarkup(markupsInChildNodes)
+      );
+    }
+    text += child.textContent;
+    currCol += child.textContent.length;
+  }
+  return {
+    text,
+    markups,
+  };
 }
